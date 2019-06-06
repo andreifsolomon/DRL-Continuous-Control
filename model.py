d4fedd85cfeae4055c585ff5c019a6b1d883e475
andreifsolomon <andrei.f.solomon@gmail.com>
2019-06-06 00:35:14 -0400

    refactor: remove useless comments from model.py

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=256, fc2_units=128, fc3_units=128):
+    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=128):
         """Initialize parameters and build model.
         Params
         ======
@@ -25,39 +25,29 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
 
-        # self.norm1 = torch.nn.BatchNorm1d(state_size)
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.norm2 = torch.nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        # self.norm3 = torch.nn.BatchNorm1d(fc2_units)
-        # self.fc4 = nn.Linear(fc2_units, action_size)
-        # self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc3_units, action_size)
+        self.fc3 = nn.Linear(fc2_units, action_size)
         self.reset_parameters()
 
     def reset_parameters(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        # self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-        self.fc4.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        # state = self.norm1(state)
-        x = F.elu(self.fc1(state))
+        x = F.relu(self.fc1(state))
         x = self.norm2(x)
-        x = F.elu(self.fc2(x))
-        # x = self.norm3(x)
-
-        # x = F.leaky_relu(self.fc3(x))
-        return torch.tanh(self.fc4(x))
-        # return F.tanh(self.fc4(x))
+        x = F.relu(self.fc2(x))
+        return torch.tanh(self.fc3(x))
 
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=256, fc2_units=128, fc3_units=128):
+    def __init__(self, state_size, action_size, seed, fcs1_units=128, fc2_units=128):
         """Initialize parameters and build model.
         Params
         ======
@@ -69,31 +59,22 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        # self.norm1 = torch.nn.BatchNorm1d(state_size)
+
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.norm2 = torch.nn.BatchNorm1d(fcs1_units)
         self.fc2 = nn.Linear(fcs1_units + action_size, fc2_units)
-        # self.norm3 = torch.nn.BatchNorm1d(fc2_units)
-        # self.fc4 = nn.Linear(fc2_units, 1)
-
-        # self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc3_units, 1)
+        self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
         self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        # self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-        self.fc4.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        # state = self.norm1(state)
-        xs = F.elu(self.fcs1(state))
+        xs = F.relu(self.fcs1(state))
         xs = self.norm2(xs)
         x = torch.cat((xs, action.float() ), dim=1)
-        x = F.elu(self.fc2(x))
-        # x = self.norm3(x)
-
-        # x = F.leaky_relu(self.fc3(x))
-        return self.fc4(x)
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
